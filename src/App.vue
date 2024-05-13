c970c56a0488377673bc7a3603276ff6b4e9d0bc
jinmingYuana <jinming.yuan@qq.com>
2024-05-13 10:08:27 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  {{ msg }}{{ num }}{{ ok?"yes":"no" }}
+
+  0
+</template>
+<script>
+export default{
+  data(){
+    return{
+      msg:"神奇的语法",
+      num:1+1,
+      ok:true
+    }
+  }
+}  
+</script>
